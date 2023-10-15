c7ad7eb4a629ced6cad1264237fd0db4f7051c1d
Clu <ncom3000@gmail.com>
2023-10-15 19:47:51 +0300

    Probe binary not found temporary workaround.

--- src/Service/VideoService.php
+++ src/Service/VideoService.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Flynn314\Matrix\Service;
 
+use FFMpeg\Exception\ExecutableNotFoundException;
 use FFMpeg\FFProbe;
 use Flynn314\Matrix\Entity\VideoData;
 
@@ -10,17 +11,21 @@ final class VideoService
 {
     public function getVideoDataByFilepath(string $filepath): VideoData
     {
-        $probe = FFProbe::create();
-        $dimension = $probe
-            ->streams($filepath) // extracts streams information
-            ->videos() // filters video streams
-            ->first() // returns the first video stream
-            ->getDimensions();
+        try {
+            $probe = FFProbe::create();
+            $dimension = $probe
+                ->streams($filepath) // extracts streams information
+                ->videos() // filters video streams
+                ->first() // returns the first video stream
+                ->getDimensions();
 
-        return new VideoData(
-            $dimension->getWidth(),
-            $dimension->getHeight(),
-            intval($probe->format($filepath)->get('duration') * 1000)
-        );
+            return new VideoData(
+                $dimension->getWidth(),
+                $dimension->getHeight(),
+                intval($probe->format($filepath)->get('duration') * 1000)
+            );
+        } catch (ExecutableNotFoundException) {
+            return new VideoData(0, 0, 0);
+        }
     }
 }
